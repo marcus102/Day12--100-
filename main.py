507a727bc73a1742b16362f949bc970919739f72
Sawadogo Marcus <wendpanga2002@gmail.com>
2023-03-05 15:20:10 +0100

    code.

--- main.py
+++ main.py
@@ -20,19 +20,25 @@ else:
     max_attempt -= 5
 print(f"You have {max_attempt} remaining to guess the number")
 
-while final_attempt < max_attempt:
+while final_attempt != max_attempt:
+    
     max_attempt -= 1
     number_guessed = int(input("Make a guess: "))
     if number_guessed == correct_answer:
         print(f"You got it! The answer was {correct_answer} ")
         final_attempt = max_attempt
     else:
-        if number_guessed == correct_answer:
-            print("You've run out of guess, you lose!")
-            
+        if number_guessed < correct_answer:
+            if max_attempt ==0 :
+                print("You've run out of guess, you lose!")
+            else:
+                print("Too low.\nGuess again: ")
+                print(f"You have {max_attempt} remaining to guess the number") 
+                
         elif number_guessed > correct_answer:
-            print("Too high.\nGuess again: ")
-            print(f"You have {max_attempt} remaining to guess the number") 
-        else:
-            print("Too low.\nGuess again: ")
-            print(f"You have {max_attempt} remaining to guess the number")      
+            if max_attempt ==0 :
+                print("You've run out of guess, you lose!")
+            else:
+                print("Too high.\nGuess again: ")
+                print(f"You have {max_attempt} remaining to guess the number") 
+                    
